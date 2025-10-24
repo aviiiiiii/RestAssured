b1062a8285824924e5a154de2be9e53903095a41
Devavikashini09 <apdevavikashini@gmail.com>
2025-10-24 16:27:16 +0530

    Allure report integration

--- src/test/java/com/example/api/tests/ApiVersioningTest.java
+++ src/test/java/com/example/api/tests/ApiVersioningTest.java
@@ -1,114 +1,144 @@
-package com.example.api.tests;
+    package com.example.api.tests;
 
-import com.example.api.utils.ExcelUtils;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+    import com.example.api.utils.ExcelUtils;
+    import io.qameta.allure.*;
+    import io.restassured.RestAssured;
+    import io.restassured.http.ContentType;
+    import io.restassured.path.json.JsonPath;
+    import io.restassured.response.Response;
+    import org.testng.Assert;
+    import org.testng.annotations.*;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+    import java.io.IOException;
+    import java.nio.file.Files;
+    import java.nio.file.Path;
+    import java.util.Arrays;
+    import java.util.List;
+    import java.util.Map;
+    import java.util.Set;
+    import java.util.stream.Collectors;
 
-public class ApiVersioningTest {
+    @Epic("API Versoning Tests")
+    @Feature("GET  and POST API Validation")
+    public class ApiVersioningTest {
 
-    private Set<String> targetVersions;
-
-    @Parameters("version")
-    @BeforeTest
-    public void initVersion(String version) {
-        // Split by comma to allow multiple versions like "v1,v2"
-        targetVersions = Arrays.stream(version.split(","))
-                .map(String::trim)
-                .collect(Collectors.toSet());
-    }
-
-    @DataProvider(name = "getApiData")
-    public Object[][] getData() throws Exception {
-        String excelPath = "src/test/resources/SampleTestData.xlsx";
-        String sheetName = "GET";
-        List<Map<String, String>> data = ExcelUtils.readExcelData(excelPath, sheetName);
-
-        return data.stream()
-                .filter(row -> targetVersions.contains(row.get("Version")))
-                .map(row -> new Object[]{row})
-                .toArray(Object[][]::new);
-    }
-
-    @DataProvider(name = "postApiData")
-    public Object[][] getData_2() throws Exception {
-        String excelPath = "src/test/resources/SampleTestData.xlsx";
-        String sheetName = "POST";
-        List<Map<String, String>> data = ExcelUtils.readExcelData(excelPath, sheetName);
-
-        return data.stream()
-                .filter(row -> targetVersions.contains(row.get("Version")))
-                .map(row -> new Object[]{row})
-                .toArray(Object[][]::new);
+        private Set<String> targetVersions;
 
 
-        //        Object[][] obj = new Object[data.size()][1];
-//        for (int i = 0; i < data.size(); i++) {
-//            obj[i][0] = data.get(i);
-//        }
-//        return obj;
-    }
+        @Parameters("version")
+        @BeforeTest
+        @Step("Initialize target API versions")
+        public void initVersion(String version) {
+            // Split by comma to allow multiple versions like "v1,v2"
+            targetVersions = Arrays.stream(version.split(","))
+                    .map(String::trim)
+                    .collect(Collectors.toSet());
+            Allure.step("Target versions initialized: " + targetVersions);
+        }
 
-    @Test(dataProvider = "getApiData")
-    public void getMethods(Map<String, String> row){
-        RestAssured.baseURI = "http://localhost:5051";
+        @DataProvider(name = "getApiData")
+        public Object[][] getData() throws Exception {
+            String excelPath = "src/test/resources/SampleTestData.xlsx";
+            String sheetName = "GET";
+            List<Map<String, String>> data = ExcelUtils.readExcelData(excelPath, sheetName);
 
-        String endpoint = row.get("Endpoint");
-        String method = row.get("Method");
-        String version = row.get("Version");
-        String outputData = row.get("OutputData");
-        int expectedStatus = Integer.parseInt(row.get("ExpectedStatus"));
+            return data.stream()
+                    .filter(row -> targetVersions.contains(row.get("Version")))
+                    .map(row -> new Object[]{row})
+                    .toArray(Object[][]::new);
+        }
 
-        Response response = RestAssured.given().when().request(method, version+endpoint).then().extract().response();
-        Assert.assertEquals(response.getStatusCode(), expectedStatus, "Status code mismatch for " + row.get("TestCase"));
-        JsonPath jsonPath = response.jsonPath();
-        String out = jsonPath.getString("data");
-        Assert.assertEquals(out, outputData, "Output data mismatch for " + row.get("TestCase"));
-        System.out.println(response.body().asString());
-    }
+        @DataProvider(name = "postApiData")
+        public Object[][] getData_2() throws Exception {
+            String excelPath = "src/test/resources/SampleTestData.xlsx";
+            String sheetName = "POST";
+            List<Map<String, String>> data = ExcelUtils.readExcelData(excelPath, sheetName);
+
+            return data.stream()
+                    .filter(row -> targetVersions.contains(row.get("Version")))
+                    .map(row -> new Object[]{row})
+                    .toArray(Object[][]::new);
+
+        }
+
+        @Test(dataProvider = "getApiData", description = "Verify GET API responses for each version")
+        @Severity(SeverityLevel.CRITICAL)
+        @Story("GET API Versioning Test")
+        @Description("Validates GET endpoints for multiple API versions using data from Excel.")
+        public void getMethods(Map<String, String> row){
+            RestAssured.baseURI = "http://localhost:5051";
 
 
+            String endpoint = row.get("Endpoint");
+            String method = row.get("Method");
+            String version = row.get("Version");
+            String outputData = row.get("OutputData");
+            int expectedStatus = Integer.parseInt(row.get("ExpectedStatus"));
 
-    @Test(dataProvider = "postApiData")
-    public void postMethods(Map<String, String> row) throws IOException {
-        RestAssured.baseURI = "http://localhost:5051";
+            Allure.parameter("Version", version);
+            Allure.parameter("Endpoint", endpoint);
+            Allure.parameter("TestCase", row.get("TestCase"));
 
-        String endpoint = row.get("Endpoint");
-        String method = row.get("Method");
-        String version = row.get("Version");
-        String outputData = row.get("OutputData");
-        int expectedStatus = Integer.parseInt(row.get("ExpectedStatus"));
+            Allure.step("Sending GET request to " + version + endpoint);
+            Response response = RestAssured.given()
+                    .when()
+                    .request(method, version + endpoint)
+                    .then()
+                    .extract()
+                    .response();
 
+            Allure.step("Validating response status and data");
+            Allure.addAttachment("Response Body", "application/json", response.getBody().asPrettyString());
 
-        var request = RestAssured.given().contentType(ContentType.JSON);
-
-        String inputFile = row.get("InputFile");
-        if (inputFile != null && !inputFile.isBlank()) {
-            String payload = Files.readString(Path.of("src/test/resources/payloads/" + inputFile));
-            request = request.body(payload);
+            Assert.assertEquals(response.getStatusCode(), expectedStatus, "Status code mismatch for " + row.get("TestCase"));
+            JsonPath jsonPath = response.jsonPath();
+            String out = jsonPath.getString("data");
+            Assert.assertEquals(out, outputData, "Output data mismatch for " + row.get("TestCase"));
         }
 
 
-        Response response = request.when().request(method, version+endpoint).then().extract().response();
-        Assert.assertEquals(response.getStatusCode(), expectedStatus, "Status code mismatch for " + row.get("TestCase"));
-        JsonPath jsonPath = response.jsonPath();
-        String out = jsonPath.getString("data");
-        Assert.assertEquals(out, outputData, "Output data mismatch for " + row.get("TestCase"));
-        System.out.println(response.body().asString());
+
+        @Test(dataProvider = "postApiData", description = "Verify POST API responses for each version")
+        @Severity(SeverityLevel.BLOCKER)
+        @Story("POST API Versioning Test")
+        @Description("Validates POST endpoints for multiple API versions using data from Excel.")
+        public void postMethods(Map<String, String> row) throws IOException {
+            RestAssured.baseURI = "http://localhost:5051";
+
+            String endpoint = row.get("Endpoint");
+            String method = row.get("Method");
+            String version = row.get("Version");
+            String outputData = row.get("OutputData");
+            int expectedStatus = Integer.parseInt(row.get("ExpectedStatus"));
+
+
+            Allure.parameter("Version", version);
+            Allure.parameter("Endpoint", endpoint);
+            Allure.parameter("TestCase", row.get("TestCase"));
+
+            var request = RestAssured.given().contentType(ContentType.JSON);
+
+            String inputFile = row.get("InputFile");
+            if (inputFile != null && !inputFile.isBlank()) {
+                String payloadPath = "src/test/resources/payloads/" + inputFile;
+                String payload = Files.readString(Path.of(payloadPath));
+                Allure.addAttachment("Request Payload", "application/json", payload);
+                request = request.body(payload);
+            }
+
+            Allure.step("Sending POST request to " + version + endpoint);
+            Response response = request.when()
+                    .request(method, version + endpoint)
+                    .then()
+                    .extract()
+                    .response();
+
+            Allure.step("Validating response");
+            Allure.addAttachment("Response Body", "application/json", response.getBody().asPrettyString());
+
+            Assert.assertEquals(response.getStatusCode(), expectedStatus, "Status code mismatch for " + row.get("TestCase"));
+            JsonPath jsonPath = response.jsonPath();
+            String out = jsonPath.getString("data");
+            Assert.assertEquals(out, outputData, "Output data mismatch for " + row.get("TestCase"));
+        }
     }
-}
